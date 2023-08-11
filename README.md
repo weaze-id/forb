dee4374d25fabb454334651a4f8529095d7e202e
Edwin Sulaiman <edwinsulaiman20@gmail.com>
2023-08-11 16:04:09 +0700

    build: update rollup config

--- README.md
+++ README.md
@@ -144,4 +144,4 @@ Contributions to **forb** are welcome! To contribute, please follow the guidelin
 
 ## License
 
-This project is licensed under the MIT License - see the [LICENSE](LICENSE.md) file for details.
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import terser from "@rollup/plugin-terser";
 
 import packageJson from "./package.json" assert { type: "json" };
 
@@ -18,7 +19,8 @@ export default [
         format: "esm",
       },
     ],
-    plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" })],
+    plugins: [resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.json" }), terser()],
+    external: ["react", "react-dom"],
   },
   {
     input: "dist/esm/types/index.d.ts",
